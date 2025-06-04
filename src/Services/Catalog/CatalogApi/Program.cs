76348f87fc6a7fbb3e8968e5ea82b62a014ab8f4
Shubham Kumar <shubham.eu.kumar@accenture.com>
2025-06-04 19:09:18 +0530

    Post test

--- src/Services/Catalog/CatalogApi/Program.cs
+++ src/Services/Catalog/CatalogApi/Program.cs
@@ -1,9 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Ad service 
+// Add service 
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request
+app.MapCarter();
 
 app.Run();
